16aeb6c282c3da971ce70bcfed1b31448d593891
Chad Moon <cmoon@kenzan.com>
2015-10-05 11:13:23 -0600

    updates

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ rm -rf deploy/classes
 mvn dependency:tree
 mvn clean install
 mvn package
-cp target/myprojecls
-t-0.0.1-SNAPSHOT-boot.jar deploy/service.jar
+cp target/myproject-0.0.1-SNAPSHOT-boot.jar deploy/service.jar
