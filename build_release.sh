a9ea93a47b6b1ad67285a121debf82da398ab6fc
Chad Moon <cmoon@kenzan.com>
2015-10-05 11:05:41 -0600

    build

--- build_release.sh
+++ build_release.sh
@@ -6,10 +6,11 @@ git checkout master -- deploy
 git checkout master -- src
 git checkout master -- pom.xml
 git checkout master -- build.sh
-./build.sh
-rm -rf target
-git add .
-git add -u
-git commit -m 'build'
-git push origin builds
-git checkout master
+
+#./build.sh
+#rm -rf target
+#git add .
+#git add -u
+#git commit -m 'build'
+#git push origin builds
+#git checkout master
