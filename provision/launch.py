ad1f0a172659ba2f45ac1c0b501079014fc9d327
Chad Moon <cmoon@kenzan.com>
2015-10-05 14:11:27 -0600

    files

--- provision/launch.py
+++ provision/launch.py
@@ -3,7 +3,7 @@ import os
 import time
 
 #grab ami id of last built image
-images = os.popen("aws ec2 describe-images --filters Name=tag-key,Values=type Name=tag-value,Values=mavenapp --query 'Images[*].{ID:ImageId}'").read()
+images = os.popen("aws ec2 describe-images --filters Name=tag-key,Values=type Name=tag-value,Values=springapp --query 'Images[*].{ID:ImageId}'").read()
 images = json.loads(images)
 ami = images[0]['ID']
 
@@ -20,12 +20,14 @@ ami = images[0]['ID']
 #wait until instance has launched to create asg
 #time.sleep(120)
 
+#ec2-run-instances --key KEYPAIR --user-data-file install-lamp ami-bf5eb9d6
+
 #create launch configuration
 launchconfig = "launch" + ami
-print os.popen("aws autoscaling create-launch-configuration --launch-configuration-name " + launchconfig + " --key-name cmoon --image-id " + ami + " --instance-type t2.micro --security-groups 'wide open'").read()
+print os.popen("aws autoscaling create-launch-configuration --launch-configuration-name " + launchconfig + " --user-data user-data.sh --key-name cmoon --image-id " + ami + " --instance-type t2.micro --security-groups 'wide open'").read()
 
 
 #create asg
-print os.popen("aws autoscaling create-auto-scaling-group --auto-scaling-group-name mavenapp-web-dev --launch-configuration-name " + launchconfig + " --min-size 2 --max-size 2 --desired-capacity 2 --vpc-zone-identifier subnet-88fb53ed  --load-balancer-names 'Tomcat' --health-check-type ELB --health-check-grace-period 30").read()
+print os.popen("aws autoscaling create-auto-scaling-group --auto-scaling-group-name springapp-web-dev --launch-configuration-name " + launchconfig + " --min-size 2 --max-size 2 --desired-capacity 2 --vpc-zone-identifier subnet-88fb53ed  --load-balancer-names 'Tomcat' --health-check-type ELB --health-check-grace-period 30").read()
 
 #terminate parent instance
